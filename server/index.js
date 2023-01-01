84f708a24fd1a7c95581037d2eb1d9956fe25bb7
Jared Mullins <jared.mullins@coxautoinc.com>
2023-01-01 14:02:08 -0600

    updated ports

--- server/index.js
+++ server/index.js
@@ -140,7 +140,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 
 
 app.listen(port, () => {
